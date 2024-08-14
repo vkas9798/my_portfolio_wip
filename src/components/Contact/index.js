d4feede047a0b062c7434b50fed94c70b5fa357f
vkas9798 <sharmavikas.9798@gmail.com>
2024-08-14 11:08:25 +0530

    form update

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -130,7 +130,7 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    emailjs.sendForm('service_tox7kqs', 'template_nv7k7mj', form.current, 'SybVGsYS52j2TfLbi')
+    emailjs.sendForm('service_wd550ru', 'template_9d3ufdn', form.current, 'SybVGsYS52j2TfLbi')
       .then((result) => {
         setOpen(true);
         form.current.reset();
